7e0ca105cc60f21dc6a7cb84a193b09e5d025dba
Roman Tuomisto <romantuomisto@gmail.com>
2022-04-09 08:32:29 +0300

    create vue app, and add sass support

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { RouterLink } from "vue-router";
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '@/assets/scss/header.scss';
+</style>
